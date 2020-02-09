062f86f3278f6bc66f9360c0c4d75aa57d4cdadf
Josh Zhou <joshzhou745@gmail.com>
2020-02-09 16:24:12 -0500

    first commit

--- app.py
+++ app.py
@@ -28,10 +28,6 @@ app.config['APP_CONSUMER_KEY'] = os.getenv(
 app.config['APP_CONSUMER_SECRET'] = os.getenv(
     'TWAUTH_APP_CONSUMER_SECRET', 'API_Secret_from_Twitter')
 
-# alternatively, add your key and secret to config.cfg
-# config.cfg should look like:
-# APP_CONSUMER_KEY = 'API_Key_from_Twitter'
-# APP_CONSUMER_SECRET = 'API_Secret_from_Twitter'
 app.config.from_pyfile('config.cfg', silent=True)
 
 oauth_store = {}
@@ -62,32 +58,7 @@ def hello():
     oauth_token_secret = request_token[b'oauth_token_secret'].decode('utf-8')
     oauth_store[oauth_token] = oauth_token_secret
     return render_template('start.html', authorize_url=authorize_url, oauth_token=oauth_token, request_token_url=request_token_url)
-    # print(app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'],'hereeee')
-    # return render_template('index.html')
-
-
-# @app.route('/start')
-# def start():
-#     # note that the external callback URL must be added to the whitelist on
-#     # the developer.twitter.com portal, inside the app settings
-#     app_callback_url = url_for('callback', _external=True)
-
-#     # Generate the OAuth request tokens, then display them
-#     consumer = oauth.Consumer(
-#         app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'])
-#     client = oauth.Client(consumer)
-#     resp, content = client.request(request_token_url, "POST", body=urllib.parse.urlencode({"oauth_callback": app_callback_url}))
-
-#     if resp['status'] != '200':
-#         error_message = 'Invalid response, status {status}, {message}'.format(
-#             status=resp['status'], message=content.decode('utf-8'))
-#         return render_template('error.html', error_message=error_message)
-
-#     request_token = dict(urllib.parse.parse_qsl(content))
-#     oauth_token = request_token[b'oauth_token'].decode('utf-8')
-#     oauth_token_secret = request_token[b'oauth_token_secret'].decode('utf-8')
-#     oauth_store[oauth_token] = oauth_token_secret
-#     return render_template('start.html', authorize_url=authorize_url, oauth_token=oauth_token, request_token_url=request_token_url)
+  
 
 
 
@@ -158,33 +129,32 @@ def callback():
 
 
    
-    # don't keep this token and secret in memory any longer
-    # del oauth_store[oauth_token]
+  
     global api
     api = twitter.Api(consumer_key = consumer_key, consumer_secret = consumer_secret_key, access_token_key = real_oauth_token, access_token_secret = real_oauth_token_secret)
-    #api.PostDirectMessage("hola", screen_name="steelcitycodes")
+
     return redirect(url_for("user", screenname = screen_name))
-    # return render_template('callback-success.html', screen_name=screen_name, user_id=user_id, name=name,
-    #                        friends_count=friends_count, statuses_count=statuses_count, followers_count=followers_count, access_token_url=access_token_url)
-
-
+   
 
 
 loaded_model = pickle.load(open('newmodel.sav', 'rb'))
 vectorizer = pickle.load(open('newvectorizer.pickle', 'rb'))
 
 def get_mutual_friends(user):
-  global api
-  friends = api.GetFriends(screen_name = user)
-  followers = api.GetFollowers(screen_name = user)
-  #print(friends[0].screen_name)
-  friendnames = []
-  for friend in friends:
-    if(friend in followers):
-        friendnames.append(friend)
-  if(len(friendnames) > 20):
-    friendnames = friendnames[:21]
-  return friendnames
+  try:
+    global api
+    friends = api.GetFriends(screen_name = user)
+    followers = api.GetFollowers(screen_name = user)
+    #print(friends[0].screen_name)
+    friendnames = []
+    for friend in friends:
+      if(friend in followers):
+          friendnames.append(friend)
+    if(len(friendnames) > 20):
+      friendnames = friendnames[:21]
+    return friendnames
+  except:
+    return render_template('error.html', error_message='Rate limit exceeded. Too many API calls in 15 min. period'), 500
 
 
 
@@ -242,7 +212,7 @@ def getResults():
         datas = request.get_json()
         print(datas)
         allData = get_friends_data(datas['username'])
-        #llData = get_friends_data(request.dat)
+        
     return allData
 
 
@@ -252,13 +222,10 @@ def getResults():
 
 @app.route('/user/<screenname>')
 def user(screenname):
-    #api = twitter.Api(consumer_key = app.config['APP_CONSUMER_KEY'], consumer_secret = app.config['APP_CONSUMER_SECRET'], access_token_key = access_tokens[0], access_token_secret = access_tokens[1])
-    #api.PostDirectMessage("test2", screen_name="joshjzhou")
+    
     global api
     name = api.GetUser(screen_name = screenname).name
-    # followers = get_mutual_friends(screenname)
-    # for follower in followers:
-    #     print(follower.screen_name)
+   
     return render_template('index.html', screenname = screenname, name = name)
 
 
